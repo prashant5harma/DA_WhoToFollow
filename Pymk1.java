ef55a23be908f50db952f4b114290540925a34ab
prashant5harma <mr.prashansharma@gmail.com>
2017-02-09 14:18:53 -0600

    First Mapper

--- Pymk1.java
+++ Pymk1.java
@@ -1,4 +1,4 @@
-package pymk.pkg1;
+//package pymk.pkg1;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -30,7 +30,7 @@ public class Pymk1 {
 
         public void map(Object key, Text values, Context context) throws IOException, InterruptedException {
             StringTokenizer st = new StringTokenizer(values.toString());
-            IntWritable user = new IntWritable(Integer.parseInt(st.nextToken()));
+            IntWritable firstUser = new IntWritable(Integer.parseInt(st.nextToken()));
             // 'friends' will store the list of friends of user 'user'
             ArrayList<Integer> friends = new ArrayList<>();
             // First, go through the list of all friends of user 'user' and emit 
@@ -38,18 +38,18 @@ public class Pymk1 {
             // 'friend1' will be used in the emitted pair
             IntWritable friend1 = new IntWritable();
             while (st.hasMoreTokens()) {
-                Integer friend = Integer.parseInt(st.nextToken());
-                friend1.set(-friend);
-                context.write(user, friend1);
-                friends.add(friend); // save the friends of user 'user' for later
+                IntWritable followed = new IntWritable(Integer.parseInt(st.nextToken()));
+                //friend1.set(-friend);
+                context.write(followed, firstUser);
+                //friends.add(friend); // save the friends of user 'user' for later
             }
             // Now we can emit all (a,b) and (b,a) pairs
             // where a!=b and a & b are friends of user 'user'.
             // We use the same algorithm as before.
-            ArrayList<Integer> seenFriends = new ArrayList<>();
+            //ArrayList<Integer> seenFriends = new ArrayList<>();
             // The element in the pairs that will be emitted.
-            IntWritable friend2 = new IntWritable();
-            for (Integer friend : friends) {
+            //IntWritable friend2 = new IntWritable();
+          /*  for (Integer friend : friends) {
                 friend1.set(friend);
                 for (Integer seenFriend : seenFriends) {
                     friend2.set(seenFriend);
@@ -57,7 +57,7 @@ public class Pymk1 {
                     context.write(friend2, friend1);
                 }
                 seenFriends.add(friend1.get());
-            }
+            } */
         }
     }
 
@@ -113,6 +113,50 @@ public class Pymk1 {
         // The reduce method       
         public void reduce(IntWritable key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
             IntWritable user = key;
+            ArrayList<Integer> existingFollowers = new ArrayList();
+            // 'recommendedUsers' will store the list of user ids recommended
+            // to user 'user'
+            ArrayList<Integer> recommendedFollowers = new ArrayList<>();
+            while (values.iterator().hasNext()) {
+                int value = values.iterator().next().get();
+                if (value > 0) {
+                	recommendedFollowers.add(value);
+                } else {
+                	existingFollowers.add(value);
+                }
+            }
+            
+            ArrayList<Recommendation> recommendations = new ArrayList<>();
+            // Builds the recommendation array
+            for (Integer userId : recommendedFollowers) {
+                Recommendation p = Recommendation.find(userId, recommendations);
+                if (p == null) {
+                    recommendations.add(new Recommendation(userId));
+                } else {
+                    p.addCommonFriend();
+                }
+            }
+         // Builds the output string that will be emitted
+            StringBuffer sb = new StringBuffer(""); // Using a StringBuffer is more efficient than concatenating strings
+            for (int i = 0; i < recommendations.size() && i < 10; i++) {
+                Recommendation p = recommendations.get(i);
+                sb.append(p.toString() + " ");
+            }
+            Text res = new Text(sb.toString());
+            context.write(user, res);
+        
+            
+         // Builds the output string that will be emitted
+           /*  // Using a StringBuffer is more efficient than concatenating strings
+            while(values.hasNext()) 
+            {
+                sb.append(values.toString() + " ");
+            }
+            Text result = new Text(sb.toString());
+            context.write(user, result);*/
+           
+            
+            /*
             // 'existingFriends' will store the friends of user 'user'
             // (the negative values in 'values').
             ArrayList<Integer> existingFriends = new ArrayList();
@@ -164,6 +208,8 @@ public class Pymk1 {
             }
             Text result = new Text(sb.toString());
             context.write(user, result);
+        }
+        */
         }
     }
 
